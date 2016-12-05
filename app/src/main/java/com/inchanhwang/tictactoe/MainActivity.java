2d920ca8656321228c2bab07c47641d32affe608
inchanhwang <neogeo-s@hanmail.net>
2016-12-05 13:11:49 +0900

    Works but playagain button crashes

---
+++ app/src/main/java/com/inchanhwang/tictactoe/MainActivity.java
@@ -0,0 +1,98 @@
+package com.inchanhwang.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayout;
+import android.text.Layout;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 0 = yellow, 1 = red,
+
+    int activePlayer = 0;
+
+
+    // 2 means unplayed
+
+    int [] gameState = {2,2,2,2,2,2,2,2,2};
+    int [][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6} };
+
+    public void restart(View view){
+        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+        layout.setVisibility(View.INVISIBLE);
+        activePlayer = 0;
+
+        for (int i = 0; i<gameState.length; i++){
+            gameState[i] = 2;
+        }
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayoutme);
+        for(int i = 0; i<gridLayout.getChildCount(); i++){
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+        }
+    }
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        System.out.println(counter.getTag().toString());
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if (gameState[tappedCounter] == 2) {
+
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1000f);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.cross);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.redcircle);
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]] != 2) {
+                    System.out.println(gameState[winningPosition[0]]);
+                    // someone as one
+
+                    String winner = "Red";
+
+                    if (gameState[winningPosition[0]] == 0) {
+                        winner = "Cross";
+                    }
+
+
+                    TextView winnerMsge = (TextView) findViewById(R.id.winnerMsg);
+
+                    winnerMsge.setText(winner + " has won!");
+
+
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+
+
+                    layout.setVisibility(View.VISIBLE);
+
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
